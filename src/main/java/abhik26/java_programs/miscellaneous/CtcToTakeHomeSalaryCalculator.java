57504af1887fcd7f68d9f8ca849f53389df2c689
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2026-07-02 21:18:31 +0530

    Refactor salary calculation logic and improve output formatting

--- src/main/java/abhik26/java_programs/miscellaneous/CtcToTakeHomeSalaryCalculator.java
+++ src/main/java/abhik26/java_programs/miscellaneous/CtcToTakeHomeSalaryCalculator.java
@@ -6,6 +6,14 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CtcToTakeHomeSalaryCalculator {
+
+	// multipliers are respect to basic salary
+	private static final float grossMultiplier = 2f; // can be 2x to 2.5x of basic salary i.e. (basic is in the range of 40% to 50% of gross salary)
+	private static final float hraMultiplier = 0.4f; // can be 0.4x or 0.5x of basic salary i.e. (hra can be 40% or 50% of basic salary)
+	private static final float epfMultiplier = 0.12f;
+	private static final float gratuityMultiplier = 0.048f;
+	private static final float professionalTaxMonthly = 200f;
+
 	public static void main(String[] args) {
 		int ctc = 15_00_000;
 		calculateMonthlyTakeHomeSalary(ctc);
@@ -13,31 +21,26 @@ public class CtcToTakeHomeSalaryCalculator {
 
 	public static void calculateMonthlyTakeHomeSalary(int ctc) {
 
-		// multipliers are respect to basic salary
-		float grossMultiplier = 2f; // can be 2x to 2.5x of basic salary i.e. (basic is in the range of 40% to 50% of gross salary)
-		float hraMultiplier = 0.4f; // can be 0.4x or 0.5x of basic salary i.e. (hra can be 40% or 50% of basic salary)
-		float epfMultiplier = 0.12f;
-		float gratuityMultiplier = 0.048f;
+		float basicAnnual = ctc / (grossMultiplier + epfMultiplier + gratuityMultiplier);
+		float grossAnnual = basicAnnual * grossMultiplier;
+		float hraAnnual = basicAnnual * hraMultiplier;
+		float specialAnnual = grossAnnual - (basicAnnual + hraAnnual);
+		float epfAnnual = basicAnnual * epfMultiplier;
+		float gratuityAnnual = basicAnnual * gratuityMultiplier;
 
-		int basicAnnual = Math.round(ctc / (grossMultiplier + epfMultiplier + gratuityMultiplier));
-		int hraAnnual = Math.round(basicAnnual * hraMultiplier);
-		int grossAnnual = Math.round(basicAnnual * grossMultiplier);
-		int specialAnnual = Math.round(grossAnnual - (basicAnnual + hraAnnual));
-		int epfAnnual = Math.round(basicAnnual * epfMultiplier);
-		int gratuityAnnual = Math.round(basicAnnual * gratuityMultiplier);
-
-		int basicMonthly =  Math.round(basicAnnual / 12);
-		int hraMonthly = Math.round(hraAnnual / 12);
-		int grossMonthly = Math.round(grossAnnual / 12);
-		int specialMonthly = Math.round(specialAnnual / 12);
-		int epfMonthly = Math.round(epfAnnual / 12);
-		int gratuityMonthly = Math.round(gratuityAnnual / 12);
+		float basicMonthly =  basicAnnual / 12;
+		float grossMonthly = grossAnnual / 12;
+		float hraMonthly = hraAnnual / 12;
+		float specialMonthly = specialAnnual / 12;
+		float epfMonthly = epfAnnual / 12;
+		float gratuityMonthly = gratuityAnnual / 12;
 
 		float incomeTaxAnnual = calculateIncomeTax(grossAnnual);
 		float incomeTaxMonthly = incomeTaxAnnual / 12;
 
-		String format = "%-15s\t%10s\t%10s\n";
-		System.out.println("=================================================");
+		String format = "%-20s %15s %15s\n";
+		String line = "=====================================================";
+		System.out.println(line);
 		System.out.printf(format, "Component", "Monthly", "Annual");
 		System.out.printf(format, "-", "-", "-");
 		System.out.printf(format, "Basic", basicMonthly, basicAnnual);
@@ -50,11 +53,12 @@ public class CtcToTakeHomeSalaryCalculator {
 		System.out.printf(format, "Gratuity", gratuityMonthly, gratuityAnnual);
 		System.out.printf(format, "-", "-", "-");
 		System.out.printf(format, "Income tax", incomeTaxMonthly, incomeTaxAnnual);
+		System.out.printf(format, "Professional tax", professionalTaxMonthly, professionalTaxMonthly * 12);
 		System.out.printf(format, "-", "-", "-");
 		System.out.println(
-				"Monthly salary after epf and tax deduction: " + (grossMonthly - epfMonthly - (incomeTaxAnnual / 12)));
+				"In hand monthly salary after deductions: " + (grossMonthly - epfMonthly - incomeTaxMonthly - professionalTaxMonthly));
 		System.out.println("CTC provided: " + ctc + ", CTC calculated: " + (grossAnnual + epfAnnual + gratuityAnnual));
-		System.out.println("=================================================");
+		System.out.println(line);
 	}
 
 	private static float calculateIncomeTax(float grossAnnual) {
@@ -63,7 +67,7 @@ public class CtcToTakeHomeSalaryCalculator {
 		int standarDeduction = 75_000;
 		int marginalRelief = 75_000;
 		int taxIncomeRebateLimit = 12_00_000;
-		int healthAndEducationCessRate = 4;
+		float healthAndEducationCessMultiplier = 0.04f;
 
 		Map<Integer, Integer> incomeSlabRateMap = new HashMap<Integer, Integer>();
 		incomeSlabRateMap.put(5, 4_00_000);
@@ -78,22 +82,24 @@ public class CtcToTakeHomeSalaryCalculator {
 
 		float taxableIncome = grossAnnual - standarDeduction;
 
-		if (taxableIncome - taxIncomeRebateLimit <= marginalRelief) {
-			incomeTax = taxableIncome - taxIncomeRebateLimit;
-		} else if (taxableIncome > taxIncomeRebateLimit) {
-			while (!slabRatesSorted.isEmpty()) {
-				int slabRate = slabRatesSorted.removeFirst();
-				int slabRateIncome = incomeSlabRateMap.get(slabRate);
+		if (taxableIncome > taxIncomeRebateLimit) {
+			if (taxableIncome - taxIncomeRebateLimit <= marginalRelief) {
+				incomeTax = taxableIncome - taxIncomeRebateLimit;
+			} else {
+				while (!slabRatesSorted.isEmpty()) {
+					int slabRate = slabRatesSorted.removeFirst();
+					int slabRateIncome = incomeSlabRateMap.get(slabRate);
 
-				if (taxableIncome > slabRateIncome) {
-					float income = taxableIncome - slabRateIncome;
-					incomeTax += (income * slabRate / 100f);
-					taxableIncome = slabRateIncome;
+					if (taxableIncome > slabRateIncome) {
+						float income = taxableIncome - slabRateIncome;
+						incomeTax += (income * slabRate / 100f);
+						taxableIncome = slabRateIncome;
+					}
 				}
 			}
 		}
 
-		incomeTax += (incomeTax * healthAndEducationCessRate / 100);
+		incomeTax += (incomeTax * healthAndEducationCessMultiplier);
 		
 		return incomeTax;
 	}
